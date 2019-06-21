62b5690d46b32437b0c1908cb35a2b4fa3e21b24
Tiago Ventura <tiago.ventura@gmail.com>
2019-06-21 00:27:23 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,17 @@
 #include <iostream>
-#include <iomanip>
-#include <vector>
-#include <cstdlib>
-#include <cstdio>
-#include <ctime>
 
 #include "Buffer.hpp"
 
 int main( void )
 {
-	std::vector<unsigned char> v;
-
-	std::srand(std::time(NULL));
-
-	for( int i = 0; i < 128; i++ )
-		v.push_back( std::rand() % 256 );
-
-	Buffer buf( reinterpret_cast<const unsigned char*>(&v[0]), v.size() );
-
-	Buffer buf2( buf );
-
 	Buffer b;
 
-	b = buf2;
+	b.load_file( "/etc/hostname" );
 
-	std::cout << b << std::endl;
-	std::cout << buf << std::endl;
+	std::cout << b.base64() << std::endl;
+	std::cout << b.hex() << std::endl;
+	std::cout << b.base2() << std::endl;
+	std::cout << b.dump() << std::endl;
 
 	return 0;
 }
